8f8391f2a1959db801285862b2b09c2792ea370a
andyra787 <andyra787@gmail.com>
2022-10-28 12:45:01 -0300

    Prototipo Diseño Encuensta

--- resources/views/form_view/form.blade.php
+++ resources/views/form_view/form.blade.php
@@ -3,24 +3,27 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Encuesta</title>
+    <title >Encuesta</title>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </head>
-
+<style>
+    .card{
+        background-color: #636363;
+    }
+    .container{
+        background-color: #393D40;
+    }
+    body{
+        background-color: #393D40;
+    }
+</style>
 <body>
-    <div>
+    <div >
         <div class="container">
             <br>
             @if (count($errors) > 0)
@@ -35,8 +38,9 @@
                     </u>
                 </div>
             @endif
-            <div class="col-md-10 col-xs-12" style="align-self: center; margin-left:10%">
-                <h4>{{ $encuesta->nombre }}</h4>
+            <div class="col-md-10 col-xs-12" style="align-items: center;  margin-left:10%; justify-content: center;">
+
+                <h4 style="color:white";>{{ $encuesta->nombre }}</h4>
             </div>
 
             {!! Form::open(['route' => 'form_view.store']) !!}
@@ -45,19 +49,19 @@
             <div class="card">
                 <div class="card-body">
                     <div class="form-group
-                    col-sm-6">
+                    col-sm-4" style="color:white";>
                         {!! Form::label('email', 'Email:') !!}
                         {!! Form::text('email', null, ['class' => 'form-control']) !!}
                     </div>
                     <div>
                         <div class="form-group
-                        col-sm-12">
+                        col-sm-12"style="color:white";>
                             @foreach ($preguntas as $pr)
                                 <div class="form-group
                                 col-sm-12">
-                                    <label>{{ $pr->nombre }}</label>
+                                    <label><b>{{ $pr->nombre }}</b></label>
                                     <div class="form-group
-                                    col-sm-12">
+                                    col-sm-12"style="color:white";>
                                         @foreach ($respuestas as $r)
                                             @if ($r->preguntas_id == $pr->id)
                                                 <div>
@@ -79,8 +83,8 @@
             <br>
 
             <div class="form-group row">
-                <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary">enviar</button>
+                <div class="col-sm-8">
+                    <button type="submit" class="btn btn-primary">ENVIAR</button>
                 </div>
             </div>
             {!! Form::close() !!}
